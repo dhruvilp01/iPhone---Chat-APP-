0cccdc8aee140dc1039f1ce998f8423984223aab
Dhruvil Patel <pateldhruvil01@yahoo.com>
2018-07-11 11:34:21 -0400

    Reveal View Controller

---
+++ Chat App/Controller/ChannelVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelVC.swift
+//  Chat App
+//
+//  Created by Dhruvil Patel on 7/11/18.
+//  Copyright © 2018 Dhruvil. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+       
+    }
+}
---
+++ Chat App/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Chat App
+//
+//  Created by Dhruvil Patel on 7/11/18.
+//  Copyright © 2018 Dhruvil. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+
+}
